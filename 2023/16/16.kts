416fdd6605765f647300f9e3c5e020140741d263
Leo Laudouard <leo.laudouard@easymile.com>
2023-12-17 20:56:10 +0100

    best so far

--- 2023/16/16.kts
+++ 2023/16/16.kts
@@ -5,8 +5,8 @@ import kotlin.time.measureTime
 fun main() {
     val inputTest = getInput("inputTest.txt")
     mainMeasuringTime({ solve(inputTest) }, "InputTest")
-    //val input = getInput("input.txt")
-    //mainMeasuringTime({ solve(input) }, "Input")
+    val input = getInput("input.txt")
+    mainMeasuringTime({ solve(input) }, "Input")
 }
 main()
 
@@ -25,7 +25,7 @@ data class Input(val list: List<String>)
 enum class Side { R, L, T, B }
 data class Key(val x: Int, val y: Int, val side: Side)
 
-data class Visited(val visitedPoses: Set<Pair<Int, Int>>, val lasersHistory: List<Map<Key, Int>>)
+data class Visited(val visitedPoses: Set<Pair<Int, Int>>, val lasersHistory: List<Key>)
 
 fun getInput(fileName: String): Input {
     val file = File(fileName).readText()
@@ -34,60 +34,57 @@ fun getInput(fileName: String): Input {
 }
 
 fun solve(input: Input): Number {
-    val lasers = mutableMapOf(Key(-1, 0, R) to 1)
-    val visited = Visited(setOf(0 to 0), listOf(lasers))
+    val lasers = listOf(Key(-1, 0, R))
+    val visited = Visited(setOf(-1 to 0), lasers)
     val visitedPoses2 = recurse(lasers, input, visited)
-/*
-    var loop = true
-    while (loop) {
-        val (newLasers, newVisited) = travel(lasers, input.list, visited)
-        //&& newVisited.lasersHistory.toList()
-        //                .last() in visited.lasersHistory.toList()
-        if (newVisited.keys().distinct() == visited.keys().distinct()
-        ) {
-            loop = false
+    /*
+        var loop = true
+        while (loop) {
+            val (newLasers, newVisited) = travel(lasers, input.list, visited)
+            //&& newVisited.lasersHistory.toList()
+            //                .last() in visited.lasersHistory.toList()
+            if (newVisited.keys().distinct() == visited.keys().distinct()
+            ) {
+                loop = false
+            }
+            lasers = newLasers.toMutableMap()
+            //newVisited.keys().print(input.list.first().length, input.list.size)
+            visited = newVisited
         }
-        lasers = newLasers.toMutableMap()
-        //newVisited.keys().print(input.list.first().length, input.list.size)
-        visited = newVisited
-    }
-*/
+    */
 
-    val visitedPoses = visitedPoses2.keys()
+    val visitedPoses = visitedPoses2.lasersHistory
     visitedPoses.print(input.list.first().length, input.list.size)
     return visitedPoses.size(input.list.first().length, input.list.size)
 }
 
-tailrec fun recurse(lasers: Map<Key, Int>, input: Input, visited: Visited): Visited {
+tailrec fun recurse(lasers: List<Key>, input: Input, visited: Visited): Visited {
     val (newLasers, newVisited) = travel(lasers, input.list, visited)
-    newVisited.keys().print(input.list.first().length, input.list.size)
-    println("Travalled visited poses: ${newVisited.visitedPoses.size}, lasers count: ${newLasers.keys.size}")
-    return if (newVisited.keys().distinct() == visited.keys().distinct() ) {
+    //newVisited.keys().print(input.list.first().length, input.list.size)
+    println("Travalled visited poses: ${newVisited.visitedPoses.size}, lasers count: ${newLasers.size}")
+    return if (newVisited.lasersHistory.distinct() == visited.lasersHistory.distinct()) {
         newVisited
     } else recurse(newLasers, input, newVisited)
 }
 
-fun Visited.keys(): List<Key> = this.lasersHistory.flatMap { it.toList() }.groupBy { it.first }.map { it.key }
-
-
-fun travel(lasers: Map<Key, Int>, lines: List<String>, visited: Visited): Pair<Map<Key, Int>, Visited> {
-    return lasers.toList().fold(mapOf<Key, Int>() to visited) { (lasersAcc, visitedAcc), laser ->
-        val newPos = when (laser.first.side) {
-            T -> laser.first.x to laser.first.y - 1
-            B -> laser.first.x to laser.first.y + 1
-            R -> laser.first.x + 1 to laser.first.y
-            L -> laser.first.x - 1 to laser.first.y
+fun travel(lasers: List<Key>, lines: List<String>, visited: Visited): Pair<List<Key>, Visited> {
+    return lasers.fold(listOf<Key>() to visited) { (lasersAcc, visitedAcc), laser ->
+        val newPos = when (laser.side) {
+            T -> laser.x to laser.y - 1
+            B -> laser.x to laser.y + 1
+            R -> laser.x + 1 to laser.y
+            L -> laser.x - 1 to laser.y
         }
 
-        val newSides = getNewSides(newPos, lines, laser.first.side)
+        val newSides = getNewSides(newPos, lines, laser.side)
         val newLasers = newSides.map { side ->
-            Key(newPos.first, newPos.second, side) to 1
-        }.toMap()
-        val currentHistory = (visitedAcc.lasersHistory.lastOrNull() ?: mapOf()) + newLasers
+            Key(newPos.first, newPos.second, side)
+        }
+        val currentHistory = visitedAcc.lasersHistory + newLasers
         val newVisited = visitedAcc.copy(
-            visitedPoses = (visitedAcc.visitedPoses + newLasers.map { it.key.x to it.key.y }),
+            visitedPoses = (visitedAcc.visitedPoses + newLasers.map { it.x to it.y }),
             //lasersHistory = visitedAcc.lasersHistory + (visitedAcc.lasersHistory + newLasers)
-            lasersHistory = visitedAcc.lasersHistory + listOf(currentHistory)
+            lasersHistory = currentHistory
         )
         (lasersAcc + newLasers) to newVisited
     }
